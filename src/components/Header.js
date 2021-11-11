83497f171db95308ab51d4350b938cd51658414f
Cole Whitman <colewhitman91@gmail.com>
2021-11-10 23:53:33 -0500

    added checkboxes for filtering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,47 @@
-import './Header.module.css';
+import React, { Fragment } from 'react';
+import styles from './Header.module.css';
 const Header = () => {
-  return <header>NASA Powered Natural Event Tracker</header>;
+  return (
+    <Fragment>
+      <header>
+        NASA Powered Natural Event Tracker
+        <div className={styles.options}>
+          <input
+            type='checkbox'
+            id='wildfires'
+            name='Wildfires'
+            value='wildfires'
+            defaultChecked='checked'
+          />
+          Wildfires
+          <input
+            type='checkbox'
+            id='volcanos'
+            name='Volcanos'
+            value='volcanos'
+            defaultChecked='checked'
+          />
+          Volcanos
+          <input
+            type='checkbox'
+            id='storms'
+            name='Storms'
+            value='storms'
+            defaultChecked='checked'
+          />
+          Storms
+          <input
+            type='checkbox'
+            id='icebergs'
+            name='Icebergs'
+            value='icebergs'
+            defaultChecked='checked'
+          />
+          Icebergs
+        </div>
+      </header>
+    </Fragment>
+  );
 };
 
 export default Header;
